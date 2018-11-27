55845ae94fccc08d67b3dcac65e209e958716a4a
Jose Herranz <jose.herranz@tpvenlanube.com>
2018-11-27 12:57:22 +0100

    Display Current Location weather information for the current day

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  appId: '707b9bab12e71667f6ef288bf597ec6c',
+  baseUrl: 'http://',
 };
 
 /*
